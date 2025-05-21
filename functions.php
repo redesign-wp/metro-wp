0874f67e667b40069fcd6616593256fde4b04b9f
Jan Kristian Valgijainen <54046100+ogchibi95@users.noreply.github.com>
2025-05-21 11:31:33 +0300

    Added.

--- functions.php
+++ functions.php
@@ -1,3 +1,10 @@
-hp
+<?php
 
+if(!function_exists('theme_enqueue_scripts')){
 
+    function theme_enqueue_scripts(){
+        wp_enqueue_style('metro-wp', get_stylesheet_URI());
+    }
+}
+
+add_action('wp_enqueueu_scripts', 'theme_enqueue_scripts');
